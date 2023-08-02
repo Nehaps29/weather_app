f1d9a6ea2aa2fe4ddf69bc2a97aa0110dde98ddf
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-08-01 20:17:11 -0400

    changing the function for getting user input

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
 var key =  "934a06e15a82cd3d1449ed66cf1851d5";
-var cityName = document.getElementById("city");
+var cityName = "";
+console.log(cityName);
 //console.log(api.openweathermap.org/data/2.5/forecast)
 
+function getDetails(){
+    cityName = document.getElementById("city").value;
+   console.log(cityName);
+}
 
-
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`)
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`)
   .then(function (response) {
     return response.json();
   }) 
